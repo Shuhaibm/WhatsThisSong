dd2493402ac93239159759e7af5d2f9b7f59d46a
Shuhaibm <mesm.shuhaib@gmail.com>
2021-07-17 21:09:08 -0700

    f

--- views/index.js
+++ views/index.js
@@ -6,7 +6,7 @@ function on_click() {
     if (state.className === paused) {
         listen()
     } else if (state.className === listening) {
-        pause()
+        //pause()
     }
 }
 
@@ -18,7 +18,20 @@ function get_audio() {
             const mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start();
 
-            const allAudio = [];
+            let allAudio = [];
+
+
+            // let state = document.getElementById("button_logo")
+            // let paused = "fas fa-forward forward_icon"
+            // let listening = "fas fa-pause pause_icon"
+
+            // button.addEventListener("click", (ev) => {
+            //     if (state.className === paused) {
+            //         mediaRecorder.start()
+            //     } else if (state.className === listening) {
+            //         mediaRecorder.stop()
+            //     }
+            // })
 
             mediaRecorder.addEventListener("dataavailable", event => {
                 allAudio.push(event.data);
@@ -26,28 +39,55 @@ function get_audio() {
 
 
             mediaRecorder.addEventListener("stop", () => {
-                const finalAudio = new Blob(allAudio);
+                const finalAudio = new Blob(allAudio, { 'type': 'audio/mp4;' });
                 const audioUrl = URL.createObjectURL(finalAudio);
+                console.log(audioUrl);
+
+                audioElem = document.getElementById("audio");
+                audioElem.src = audioUrl;
+
                 const audio = new Audio(audioUrl);
 
                 audio.play();
                 pause()
 
                 //Call API from here
-
+                AudD_Call(audioUrl)
             });
 
             setTimeout(() => {
                 mediaRecorder.stop();
-            }, 3000); //FIND GOOD TIME TO USE
-
+            }, 5000); //FIND GOOD TIME TO USE
         });
-
 }
 
+function AudD_Call(audioUrl) {
+
+    var data = {
+        'api_token': '0407c995d24d78e57e6f7b80a48606bd',
+        'url': audioUrl,
+        'return': 'apple_music,spotify',
+    };
+
+    console.log(audioUrl)
 
 
 
+    // var request = new XMLHttpRequest();
+
+    // request.open('POST', "https://api.audd.io/", data);
+
+    // request.send()
+
+    // request.onload = () => {
+
+    //     console.log(request.response);
+    // }
+
+
+
+
+}
 
 
 
@@ -61,6 +101,8 @@ function pause() {
 
     state.className = paused
     document.getElementById("listen").innerHTML = "Start Listening"
+
+
 }
 
 function listen() {

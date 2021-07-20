0c0162ba5dd86baae2c8b9faaf66ebe8437e800e
Shuhaibm <mesm.shuhaib@gmail.com>
2021-07-20 14:43:16 -0700

    Everything done but spotify + apple music buttons

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function on_click() {
 
 
 
-function get_audio() {
+async function get_audio() {
     navigator.mediaDevices.getUserMedia({ audio: true })
         .then(stream => {
             const mediaRecorder = new MediaRecorder(stream);
@@ -35,14 +35,14 @@ function get_audio() {
                 pause();
                 //loading()   
                 //Call API from here
-                //result = Api_Call(finalAudio) UPDATE
-                // showResults(JSON.parse(result)); UPDATE
-                showResults("s")
+
+                Api_Call(finalAudio);
+
             });
 
             setTimeout(() => {
                 mediaRecorder.stop();
-            }, 1000); //FIND GOOD TIME TO USE UPDATE
+            }, 10000); //FIND GOOD TIME TO USE UPDATE
         });
 }
 
@@ -56,7 +56,8 @@ function Api_Call(audio) {
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
-            return this.responseText;
+            showResults(JSON.parse(this.responseText));
+            console.log(this.responseText)
         }
     });
 
@@ -88,9 +89,9 @@ function pause() {
 function listen() {
 
     let state = document.getElementById("button_logo")
-    //let listening = "fas fa-pause pause_icon"
+    let listening = "fas fa-pause pause_icon"
 
-    //state.className = listening
+    state.className = listening
     document.getElementById("listen").innerHTML = "Listening . . ."
 
     get_audio()
@@ -100,30 +101,34 @@ function listen() {
 
 
 function showResults(jsonInput) {
-    // let songTitle = jsonInput.track.title;
-    // let appleMusicURL = jsonInput.track.hub.options.actions.uri;
-    // let subtitle = jsonInput.track.subtitle;
-    // let spotifyLink = track.hub.providers.actions.uri
-
+    console.log(jsonInput)
     document.getElementById("container").style.display = 'none';
     document.getElementById("result").style.display = 'inline';
 
+    let songTitle = jsonInput.track.title;
+    let subtitle = jsonInput.track.subtitle;
+    //let appleLink = jsonInput.track.hub.options.actions.uri;
+    //let spotifyLink = jsonInput.track.hub.providers.actions.uri
 
-    let songTitle = "2040";
-    let subtitle = "le bebe";
-    let appleLink = "https://music.apple.com/gb/album/2040/1569712089?i=1569712093&ign-itscg=30201&ign-itsct=Shazam_ios&mttn3pid=a_custom_779816081798873874&mttnagencyid=769459046716559743&mttnsiteid=125115&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973";
-    let spotifyLink = "spotify:search:Voice%20of%20the%20Heroes%20Lil%20Baby"
+    // let songTitle = "2040";
+    // let subtitle = "le bebe";
+    // let appleLink = "https://music.apple.com/gb/album/2040/1569712089?i=1569712093&ign-itscg=30201&ign-itsct=Shazam_ios&mttn3pid=a_custom_779816081798873874&mttnagencyid=769459046716559743&mttnsiteid=125115&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973";
+    // let spotifyLink = "spotify:search:Voice%20of%20the%20Heroes%20Lil%20Baby"
 
 
     document.getElementById("songname2").innerHTML = songTitle;
     document.getElementById("subtitle").innerHTML = "By " + subtitle;
-    //add links
+
     document.getElementById("button_spotify").onclick = function () {
         location.href = spotifyLink;
     };
     document.getElementById("button_apple").onclick = function () {
         location.href = appleLink;
     };
+
+
+
+
 }
 
 

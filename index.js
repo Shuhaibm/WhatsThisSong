98781beb795e64999200702a9459cf8cfd94c801
Shuhaibm <mesm.shuhaib@gmail.com>
2021-07-20 19:13:48 -0700

    DONE

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function on_click() {
     let state = document.getElementById("button_logo")
     let paused = "fas fa-forward forward_icon"
-    let listening = "fas fa-pause pause_icon"
-
     loading()
 
     if (state.className === paused) {
@@ -24,7 +22,6 @@ async function get_audio() {
                 allAudio.push(event.data);
             });
 
-
             mediaRecorder.addEventListener("stop", () => {
                 var finalAudio = new Blob(allAudio, { 'type': 'audio/wav' });
                 var audioUrl = URL.createObjectURL(finalAudio);
@@ -33,16 +30,14 @@ async function get_audio() {
 
                 audio.play();
                 pause();
-                //loading()   
-                //Call API from here
 
-                Api_Call(finalAudio);
-
+                //Api_Call(finalAudio);
+                showResults() //UPDATE
             });
 
             setTimeout(() => {
                 mediaRecorder.stop();
-            }, 10000); //FIND GOOD TIME TO USE UPDATE
+            }, 100);
         });
 }
 
@@ -67,10 +62,6 @@ function Api_Call(audio) {
 
     xhr.send(data);
 }
-
-
-
-
 
 
 
@@ -101,7 +92,6 @@ function listen() {
 
 
 function showResults(jsonInput) {
-    console.log(jsonInput)
     document.getElementById("container").style.display = 'none';
     document.getElementById("result").style.display = 'inline';
 
@@ -109,13 +99,6 @@ function showResults(jsonInput) {
     let subtitle = jsonInput.track.subtitle;
     let appleLink = jsonInput.track.apple_music_url;
     let spotifyLink = jsonInput.track.hub.providers[0].actions[0].uri;
-
-    console.log(appleLink)
-    // let songTitle = "2040";
-    // let subtitle = "le bebe";
-    // let appleLink = "https://music.apple.com/gb/album/2040/1569712089?i=1569712093&ign-itscg=30201&ign-itsct=Shazam_ios&mttn3pid=a_custom_779816081798873874&mttnagencyid=769459046716559743&mttnsiteid=125115&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973";
-    // let spotifyLink = "spotify:search:Voice%20of%20the%20Heroes%20Lil%20Baby"
-
 
     document.getElementById("songname2").innerHTML = songTitle;
     document.getElementById("subtitle").innerHTML = "By " + subtitle;
@@ -126,13 +109,7 @@ function showResults(jsonInput) {
     document.getElementById("button_apple").onclick = function () {
         location.href = appleLink;
     };
-
-
-
-
 }
-
-
 
 
 function loading() {
